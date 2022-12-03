c2ba1f9db1ac6244d2d43abc5b29eeaa13359e74
HBchevelle68 <aportante10@gmail.com>
2022-12-02 22:39:28 -0500

    finish day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,26 +1,49 @@
 use std::fs;
 
 fn main() {
-    let data =
-        fs::read_to_string("/home/ap/MyRepos/AoC22/day1/test.txt").expect("Unable to read file");
+    let data = fs::read_to_string("/home/ap/MyRepos/AoC22/day1/day1_input.txt")
+        .expect("Failed to open file");
 
-    let split = data.split("\n");
+    let split = data.split('\n');
 
-    let mut total: u32 = 0;
+    let mut curr_total: u32 = 0;
     let mut top_total: u32 = 0;
-    //let mut vec_totals: Vec<u32> = vec![];
+    let mut elf_id: u32 = 1;
+
+    // .0 is elf_id
+    // .1 is total
+    let mut vec_totals: Vec<(u32, u32)> = vec![];
+
     for num in split {
+        // Found empty line
         if num.is_empty() {
-            if top_total < total {
-                top_total = total;
+            if top_total < curr_total {
+                top_total = curr_total;
             }
-            total = 0;
+            vec_totals.push((elf_id, curr_total));
+            elf_id += 1;
+            curr_total = 0;
             continue;
         }
 
-        println!("Adding {} to {}", num, total);
-        total += num.parse::<u32>().unwrap();
+        curr_total += num.parse::<u32>().unwrap();
     }
 
     println!("Top total {}", top_total);
+    vec_totals.sort_by_key(|&w| std::cmp::Reverse(w.1));
+
+    // println!(
+    //     " Top 3: {}-{} {}-{} {}-{}",
+    //     &vec_totals[0].0,
+    //     &vec_totals[0].1,
+    //     &vec_totals[1].0,
+    //     &vec_totals[1].1,
+    //     &vec_totals[2].0,
+    //     &vec_totals[2].1
+    // );
+
+    println!(
+        "Total of top 3 {}",
+        vec_totals[0].1 + vec_totals[1].1 + vec_totals[2].1
+    );
 }
