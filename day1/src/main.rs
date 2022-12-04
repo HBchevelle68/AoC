75b7cbfc016de27d573a4a5a5534bda0a12c7abc
HBchevelle68 <aportante10@gmail.com>
2022-12-04 11:03:25 -0500

    day 2 complete, minor cleaning

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,8 +1,7 @@
 use std::fs;
 
 fn main() {
-    let data = fs::read_to_string("/home/ap/MyRepos/AoC22/day1/day1_input.txt")
-        .expect("Failed to open file");
+    let data = fs::read_to_string("day1_input.txt").expect("Failed to open file");
 
     let mut curr_total: u32 = 0;
     let mut top_total: u32 = 0;
@@ -27,11 +26,11 @@ fn main() {
         curr_total += num.parse::<u32>().unwrap();
     }
 
-    println!("Top total {}", top_total);
+    println!("Part 1 total {}", top_total);
     vec_totals.sort_by_key(|&w| std::cmp::Reverse(w.1));
 
     println!(
-        "Total of top 3 {}",
+        "Part 2 top 3 {}",
         vec_totals[0].1 + vec_totals[1].1 + vec_totals[2].1
     );
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::fs;
+
 // 1st Column
 // A - Rock
 // B - Paper
@@ -11,13 +14,7 @@
 // Lose - 0 pts
 // Draw - 3 pts
 // Win  - 6 pts
-
-use std::collections::HashMap;
-use std::fs;
-
-fn main() {
-    println!("Day 2 AoC22 !");
-
+fn part1(data: &String) {
     let mut statemap: HashMap<&str, u32> = HashMap::new();
     statemap.insert("A X", 4);
     statemap.insert("A Y", 8);
@@ -31,13 +28,50 @@ fn main() {
     statemap.insert("C Y", 2);
     statemap.insert("C Z", 6);
 
-    let data = fs::read_to_string("/home/ap/MyRepos/AoC22/day2/day2_input.txt")
-        .expect("Failed to open file");
+    let total: u32 = data.lines().map(|l| statemap.get(l).unwrap()).sum();
+    println!("Part 1 Total: {}", total);
+}
 
-    let mut total: u32 = 0;
-    for line in data.lines() {
-        //println!("{}", line);
-        total += statemap.get(line).unwrap();
-    }
-    println!("Total: {}", total);
+// 1st Column
+// A - Rock
+// B - Paper
+// C - Scissors
+//
+// 2nd Column
+// X - need to lose
+// Y - need to draw
+// Z - need to win
+//
+// Rock - 1pt
+// Paper - 2pts
+// Scissors - 3pts
+//
+// Lose - 0 pts
+// Draw - 3 pts
+// Win  - 6 pts
+fn part2(data: &String) {
+    let mut statemap: HashMap<&str, u32> = HashMap::new();
+    statemap.insert("A X", 3);
+    statemap.insert("A Y", 4);
+    statemap.insert("A Z", 8);
+
+    statemap.insert("B X", 1);
+    statemap.insert("B Y", 5);
+    statemap.insert("B Z", 9);
+
+    statemap.insert("C X", 2);
+    statemap.insert("C Y", 6);
+    statemap.insert("C Z", 7);
+
+    let total: u32 = data.lines().map(|l| statemap.get(l).unwrap()).sum();
+    println!("Part 2 Total: {}", total);
+}
+
+fn main() {
+    println!("Day 2 AoC22 !");
+
+    let data = fs::read_to_string("day2_input.txt").expect("Failed to open file");
+
+    part1(&data);
+    part2(&data);
 }
