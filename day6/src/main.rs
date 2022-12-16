aab9bf9e3a24cbc9ec125d8878592b0663068f89
HBchevelle68 <aportante10@gmail.com>
2022-12-15 21:42:36 -0500

    day6 part 1 done

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,9 +1,31 @@
-use std::fs;
+use std::{fs, string};
+
+fn is_unique(s: &str) -> Option<(usize, usize, char)> {
+    s.chars().enumerate().find_map(|(i, c)| {
+        s.chars()
+            .enumerate()
+            .skip(i + 1)
+            .find(|(_, other)| c == *other)
+            .map(|(j, _)| (i, j, c))
+    })
+}
+
+fn part1(data: &String) {
+    let len = data.len();
+    let mut i = 0;
+    while (i + 3) < len {
+        let chunk = &data[i..=(i + 3)];
+        match is_unique(&chunk) {
+            None => break,
+            Some((_, _, _)) => i += 1,
+        }
+    }
+    println!("Part 1 -- first marker {}", i + 4);
+}
 
 fn main() {
     println!("Day 5 AoC22!");
 
-    let data = fs::read_to_string("test_input.txt").expect("Failed to open file");
-
-    dbg!(data);
+    let data = fs::read_to_string("day6_input.txt").expect("Failed to open file");
+    part1(&data);
 }
