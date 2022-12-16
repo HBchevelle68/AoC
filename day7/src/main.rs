779362db01967d793b90da378c9bde06bf94ca2a
HBchevelle68 <aportante10@gmail.com>
2022-12-16 16:55:42 -0500

    storing

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,31 @@
+use std::fs;
+
+#[derive(Debug)]
+enum FileType {
+    File,
+    Directory,
+}
+
+#[derive(Debug)]
+struct FileInfo {
+    name: String,
+    size: u64,
+    ftype: FileType,
+    parent:
+    subfiles: Vec<FileInfo>,
+
+}
+
+
+impl FileInfo {
+    fn change_dir(&self, dir: &str) -> FileInfo{}
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Day 7 AoC22!");
+
+    let data = fs::read_to_string("test_input.txt").expect("Failed to open file");
+
+    dbg!(&data);
 }
