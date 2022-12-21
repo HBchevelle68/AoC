13721c3083d273451a8591c4f51fe883685b7543
HBchevelle68 <aportante10@gmail.com>
2022-12-21 17:12:04 -0500

    storing

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,51 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
+
+const TESTLEN: usize = 5;
+
+fn testinput(data: &str) {
+    let mut trees: [[u8; TESTLEN]; TESTLEN] = [[0; TESTLEN]; TESTLEN];
+    let mut visable_trees: HashSet<(u8, u8)> = HashSet::new();
+
+    for (r, line) in data.lines().map(|l| l.trim()).enumerate() {
+        dbg!(&line);
+        for (c, num) in line
+            .chars()
+            .map(|cell| cell.to_digit(10).unwrap() as u8)
+            .enumerate()
+        {
+            trees[r][c] = num;
+        }
+    }
+    let mut total = (TESTLEN * 2) + ((TESTLEN - 2) * 2);
+    dbg!(&total);
+
+    for r in 1..(TESTLEN - 1) {
+        for c in 1..(TESTLEN - 1) {
+            // Left to right
+            let vis = false;
+            let tmp_tree_pos = (r, c);
+            let tmp_tree_val = trees[r][c];
+
+            for tc in 0..TESTLEN {
+
+                if (r,tc) == tmp_tree_pos{
+                    break;
+                }
+                if trees[r][tc] < tmp_tree_val
+            }
+            match vis {
+                true => {_ = visable_trees.insert(tmp_tree_pos);}
+            }
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    println!("Day 8 AoC22!");
+
+    let data = fs::read_to_string("test_input.txt").expect("Failed to open file");
+
+    testinput(&data);
 }
